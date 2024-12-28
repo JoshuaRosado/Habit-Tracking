3cfc39c3f90a18d8036652b1a1d8d36c17bc77b2
JoshuaRosado <joshua06rosado@gmail.com>
2024-12-27 19:01:11 -0600

    Initial Commit

---
+++ Habit-Tracking/Habit_TrackingApp.swift
@@ -0,0 +1,17 @@
+//
+//  Habit_TrackingApp.swift
+//  Habit-Tracking
+//
+//  Created by Joshua Rosado Olivencia on 12/27/24.
+//
+
+import SwiftUI
+
+@main
+struct Habit_TrackingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Habit-TrackingTests/Habit_TrackingTests.swift
@@ -0,0 +1,17 @@
+//
+//  Habit_TrackingTests.swift
+//  Habit-TrackingTests
+//
+//  Created by Joshua Rosado Olivencia on 12/27/24.
+//
+
+import Testing
+@testable import Habit_Tracking
+
+struct Habit_TrackingTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
