7da2a672262850ba73b433863225ba929177ee62
JoshuaRosado <joshua06rosado@gmail.com>
2024-12-27 19:27:25 -0600

    Activity Struct and class of array  created

--- Activity.swift
+++ Activity.swift
@@ -7,12 +7,21 @@
 
 import SwiftUI
 
-struct Activity: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
+struct Activity: Codable, Identifiable, Equatable {
+    var id = UUID()
+    let title: String
+    let description: String
+
 }
 
-#Preview {
-    Activity()
+@Observable
+class Activities {
+    @State private var activityList : [Activity] = []
+    
+    
+    
 }
+
+//#Preview {
+//    Activity()
+//}
