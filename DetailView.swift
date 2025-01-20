2cd7cf10c0ebfb0fd8fb97febd4aae59263d922c
JoshuaRosado <joshua06rosado@gmail.com>
2025-01-20 14:54:56 -0600

    Documentation for DetailView added

--- DetailView.swift
+++ DetailView.swift
@@ -8,10 +8,13 @@
 import SwiftUI
 
 
+// Create the main View ( DetailView )
+// Displaying a List of activities created and saved.
 
 struct DetailView: View {
-    @ObservedObject var activities = Activities()
-    @State private var showingSheet = false
+    @ObservedObject var activities = Activities()  // Observe the array of activities to update Views for any changes made by user
+    
+    @State private var showingSheet = false // Track if the DetailSheet is showing
 
     
     var body: some View {
@@ -19,6 +22,7 @@ struct DetailView: View {
         NavigationStack{
             
             List{
+                // --- HEADER ----
                 HStack{
                     Text("Title")
                     Spacer()
@@ -27,16 +31,32 @@ struct DetailView: View {
                 .font(.caption).foregroundStyle(.gray)
                 .padding(.horizontal)
                 
+                // Display each activity using it's unique ID. Use a NavigationLink for each activity to take you to the DetailSheet to display full description
                 
                 ForEach(activities.activityList, id: \.id){ activity in
                     NavigationLink(value: activity){
                         
+                        // Display a Short Preview of description in the List
+                        
+                        // 1- Get activity.Description
+                        // 2- Get first 10 characters
+                        // 3- Display first 10 characters of activity.Description
                         
                         let str = activity.description
+                        
+                        // Indexes between 0..<10 ---- //(startIndex(0)) and inner (index 10)
+                        
+                        
+                        
                         let index = str.startIndex..<str.index(str.startIndex, offsetBy: 10)
+                        
+                        // string[0..<10]
+                        // Display indexes 0..<10 from the string
                         let previewStr = str[index]
                         
                         
+                        
+                        // ---- Each ACTIVITY Row inside the List
                         HStack{
                             
                             VStack(alignment: .leading){
@@ -63,6 +83,7 @@ struct DetailView: View {
                                             .shadow(color: .black.opacity(0.5), radius: 5, x: 3.5, y: 3.5)
                                             .padding(.horizontal)
                                             
+                                        // Display individual updated count of activity completion
                                         Text("\(activity.tapCompletionCount)")
                                         
                                     }
@@ -80,12 +101,13 @@ struct DetailView: View {
                 }
                 
                 
-                
+                // Remove rows by offsets specific location
                 .onDelete(perform: removeRows)
                 
             }
             
  
+            // Each NavigationLink will have a Destination of its own Activity info
             
             .navigationDestination(for: Activity.self) { activity in
                 DetailSheet( activities: activities, title: activity.title , description: activity.description, tapCompletionCount:activity.tapCompletionCount)
@@ -97,6 +119,7 @@ struct DetailView: View {
             
             .toolbar {
                 ToolbarItem(placement: .topBarTrailing){
+                    // When Button is tapped, it will open the NewActivity Sheet View where User can add/create new activity
                     Button(action: {
                         
                         showingSheet.toggle()
@@ -113,7 +136,7 @@ struct DetailView: View {
                 }
             
             }
-            
+            // If showingSheet is TRUE = Display NewActivity Sheet
             .sheet(isPresented: $showingSheet){
                 NewActivity( activities: activities)
             }
@@ -127,7 +150,7 @@ struct DetailView: View {
         
 
     
-
+// Create a function to remove Each row at the specified offSets where is located
     func removeRows( at offsets: IndexSet ){
         activities.activityList.remove(atOffsets: offsets)
     }
--- Habit-Tracking/ContentView.swift
+++ Habit-Tracking/ContentView.swift
@@ -9,20 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        NavigationStack{
-            VStack{
-                
-                Text("Just a test")
-                Text("Just a test")
-                
-            }
-            .navigationTitle("Habit-Tacker")
-            .font(.headline)
-            
-            
-            
-            
-        }
+        DetailView()
     }
 }
 
